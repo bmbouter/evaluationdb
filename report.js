b9351d473034864158182b367d34aaa4d20952a6
Brian Bouterse <bmbouter@gmail.com>
2012-03-21 15:57:47 -0400

    Initial prototype finished and committed

---
+++ report.js
@@ -0,0 +1,156 @@
+var rows = [];
+var terms = ["schoolname", "schoolnumber", "omnibussurvey", "pd", "rla", "stem", "tle", "dst", "nctctfa", "ncvps", "strategicstaffing", "ntsp"];
+
+var getURLParameter = function(name) {
+    return decodeURIComponent((RegExp('[?|&]' + name + '=' + '(.+?)(&|#|;|$)').exec(location.search)||[,""])[1].replace(/\+/g, '%20'))||null;
+};
+
+var parseLine = function(entry) {
+    var parsed_line = {}
+
+    //Handle "Title"
+    parsed_line["leaname"] = entry.title.$t
+
+    //Handle "Content"
+    var all_terms_or_as_regex = "(" + terms.join("|") + ")";
+    for (var i = 0; i < terms.length; i++) {
+        var pattern = terms[i] + ": ((.*?), " + all_terms_or_as_regex + "|.*)";
+        regex_to_run = new RegExp(pattern,"g");
+        var match = regex_to_run.exec(entry.content.$t);
+        if (match === null) {
+            continue;
+        }
+        if (match[2] === undefined) {
+            parsed_line[terms[i]] = match[1];
+        } else {
+            parsed_line[terms[i]] = match[2];
+        }
+    }
+    return parsed_line;
+};
+
+var parseGoogleSpreadsheet = function (json) {
+    for (var i = 0; i < json.feed.entry.length; i++) {
+        var line = parseLine(json.feed.entry[i]);
+        rows.push(line);
+    }
+};
+
+var buildCheckedTextbox = function(data) {
+    var checkbox = document.createElement('input');
+    checkbox.type = "checkbox";
+    $(checkbox).click(function(){return false;});
+    if (data === "X") {
+        checkbox.checked = "True";
+    }
+    return checkbox;
+};
+
+var collectByLeaName = function() {
+    var LEAs = {};
+    for (var i = 0; i < rows.length; i++) {
+        var leaname = rows[i].leaname;
+        delete rows[i].leaname;
+        if (LEAs.hasOwnProperty(leaname) === false) {
+            LEAs[leaname] = [rows[i]];
+        } else {
+            LEAs[leaname].push(rows[i]);
+        }
+    }
+    return LEAs;
+};
+
+var render = function() {
+    var rows_by_LEAname = collectByLeaName();
+    var data_table = document.getElementById('data_table');
+
+    for(var leaname in rows_by_LEAname) {
+        var row_color = "white";
+
+        var tr = document.createElement('tr');
+        var td = document.createElement('td');
+        td.style.border = "0px";
+        var leaNameText = document.createElement('h2');
+        leaNameText.style.color = "#466089";
+        leaNameText.appendChild(document.createTextNode(leaname + ' Schools'));
+        td.appendChild(leaNameText);
+        tr.appendChild(td);
+        data_table.appendChild(tr);
+
+        for (var i = 0; i < rows_by_LEAname[leaname].length; i++) {
+            var tr = document.createElement('tr');
+
+            if (row_color === "white") {
+                row_color = "grey";
+            } else {
+                tr.style.background = "#d2d2d2"
+                row_color = "white";
+            }
+    
+            var schoolname = document.createElement('td');
+            schoolname.appendChild(document.createTextNode(rows_by_LEAname[leaname][i].schoolname));
+            tr.appendChild(schoolname);
+    
+            var schoolnumber = document.createElement('td');
+            schoolnumber.appendChild(document.createTextNode(rows_by_LEAname[leaname][i].schoolnumber));
+            tr.appendChild(schoolnumber);
+    
+            var omnibussurvey = document.createElement('td');
+            omnibussurveyCheckbox = buildCheckedTextbox(rows_by_LEAname[leaname][i].omnibussurvey);
+            omnibussurvey.appendChild(omnibussurveyCheckbox);
+            tr.appendChild(omnibussurvey);
+    
+            var pd = document.createElement('td');
+            pdCheckbox = buildCheckedTextbox(rows_by_LEAname[leaname][i].pd);
+            pd.appendChild(pdCheckbox);
+            tr.appendChild(pd);
+    
+            var rla = document.createElement('td');
+            rlaCheckbox = buildCheckedTextbox(rows_by_LEAname[leaname][i].rla);
+            rla.appendChild(rlaCheckbox);
+            tr.appendChild(rla);
+    
+            var stem = document.createElement('td');
+            stemCheckbox = buildCheckedTextbox(rows_by_LEAname[leaname][i].stem);
+            stem.appendChild(stemCheckbox);
+            tr.appendChild(stem);
+    
+            var tle = document.createElement('td');
+            tleCheckbox = buildCheckedTextbox(rows_by_LEAname[leaname][i].tle);
+            tle.appendChild(tleCheckbox);
+            tr.appendChild(tle);
+    
+            var dst = document.createElement('td');
+            dstCheckbox = buildCheckedTextbox(rows_by_LEAname[leaname][i].dst);
+            dst.appendChild(dstCheckbox);
+            tr.appendChild(dst);
+    
+            var nctctfa = document.createElement('td');
+            nctctfa.className = "greenborder leftborder topborder bottomborder"
+            nctctfaCheckbox = buildCheckedTextbox(rows_by_LEAname[leaname][i].nctctfa);
+            nctctfa.appendChild(nctctfaCheckbox);
+            tr.appendChild(nctctfa);
+    
+            var ncvps = document.createElement('td');
+            ncvps.className = "greenborder topborder bottomborder"
+            ncvpsCheckbox = buildCheckedTextbox(rows_by_LEAname[leaname][i].ncvps);
+            ncvps.appendChild(ncvpsCheckbox);
+            tr.appendChild(ncvps);
+    
+            var strategicstaffing = document.createElement('td');
+            strategicstaffing.className = "greenborder topborder bottomborder"
+            strategicstaffingCheckbox = buildCheckedTextbox(rows_by_LEAname[leaname][i].strategicstaffing);
+            strategicstaffing.appendChild(strategicstaffingCheckbox);
+            tr.appendChild(strategicstaffing);
+    
+            var ntsp = document.createElement('td');
+            ntsp.className = "greenborder topborder bottomborder rightborder"
+            ntspCheckbox = buildCheckedTextbox(rows_by_LEAname[leaname][i].ntsp);
+            ntsp.appendChild(ntspCheckbox);
+            tr.appendChild(ntsp);
+    
+            data_table.appendChild(tr);
+        }
+    }
+};
+
